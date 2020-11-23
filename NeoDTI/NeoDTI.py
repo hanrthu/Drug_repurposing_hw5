84cd7f05128ded41a6132ad6ea2a48893c35899a
韩荣 <han-r16@mails.tsinghua.edu.cn>
2020-11-23 17:50:49 +0800

    NeoDTI

--- NeoDTI/NeoDTI.py
+++ NeoDTI/NeoDTI.py
@@ -142,7 +142,7 @@ class NeoDTI(nn.Module):
         print(self.virus_virus_reconstruct_loss / virus_virus.shape[0])
         print(self.human_human_reconstruct_loss / human_human.shape[0])
         print(self.drug_protein_reconstruct_loss)
-        loss = self.drug_protein_reconstruct_loss + (self.virus_virus_reconstruct_loss / virus_virus.shape[0] + \
+        loss = self.drug_protein_reconstruct_loss + 0.1* (self.virus_virus_reconstruct_loss / virus_virus.shape[0] + \
             self.human_human_reconstruct_loss / human_human.shape[0] + self.drug_drug_reconstruct_loss / drug_drug.shape[0] + \
             self.human_human_in_reconstruct_loss / human_human.shape[0] + self.virus_human_reconstruct_loss / virus_human.shape[0]+\
             self.drug_human_reconstruct_loss / drug_human.shape[0])
